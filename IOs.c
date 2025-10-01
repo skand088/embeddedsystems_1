8579f4216337507abbe88e4473da90adfcbb1ba8
Rinad Hamid <rinad.hamid@ucalgary.ca>
2025-10-01 14:27:55 -0600

    IOinnit IOs.c

--- IOs.c
+++ IOs.c
@@ -1,1 +1,25 @@
+#include <xc.h>
 
+#define LED1 LATBbits.LATB9
+#define LED2 LATAbits.LATA6
+
+void IOinit(void) {
+    AD1PCFG = 0xFFFF;
+
+    // LED1 outputs
+    TRISBbits.TRISB9 = 0;
+    LATBbits.LATB9 = 0;
+  
+    // LED2 outputs
+    TRISAbits.TRISA6 = 0;
+    LATAbits.LATA6 = 0;
+
+    // Pushbuttons
+    TRISBbits.TRISB7 = 1; // PB1
+    TRISBbits.TRISB4 = 1; // PB2
+    TRISAbits.TRISA4 = 1; // PB3
+
+    CNPU2bits.CN23PUE = 1; // PB1
+    CNPU1bits.CN1PUE  = 1; // PB2
+    CNPU1bits.CN0PUE  = 1; // PB3
+}
