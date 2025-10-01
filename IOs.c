d1a27c910400873b9caabdb2208f14f7c27c059e
Rinad Hamid <rinad.hamid@ucalgary.ca>
2025-10-01 14:32:45 -0600

    Update IOs.c

--- IOs.c
+++ IOs.c
@@ -17,9 +17,40 @@ void IOinit(void) {
     // Pushbuttons
     TRISBbits.TRISB7 = 1; // PB1
     TRISBbits.TRISB4 = 1; // PB2
-    TRISAbits.TRISA4 = 1; // PB3
+    TRISAbits.TRISA2 = 1; // PB3
 
     CNPU2bits.CN23PUE = 1; // PB1
     CNPU1bits.CN1PUE  = 1; // PB2
-    CNPU1bits.CN0PUE  = 1; // PB3
+    CNPU1bits.CN30PUE  = 1; // PB3
 }
+
+void IOcheck(void) {
+    if(!PORTBbits.RB7 && !PORTBbits.RB4) {      // PB1 + PB2 = 1 ms blink
+        LED1 = 1;
+        delay_ms(1);
+        LED1 = 0;
+        delay_ms(1);
+    }
+    else if(!PORTBbits.RB7) {                   // PB1 = 0.25 s blink
+        LED1 = 1;
+        delay_ms(250);
+        LED1 = 0;
+        delay_ms(250);
+    }
+    else if(!PORTBbits.RB4) {                   // PB2 = 1 s blink
+        LED1 = 1;
+        delay_ms(1000);
+        LED1 = 0;
+        delay_ms(1000);
+    }
+    else if(!PORTAbits.RA2) {                   // PB3 = 6 s blink
+        LED1 = 1;
+        delay_ms(6000);
+        LED1 = 0;
+        delay_ms(6000);
+    }
+    else {
+        LED1 = 0;  // No PB pressed
+    }
+}
+
