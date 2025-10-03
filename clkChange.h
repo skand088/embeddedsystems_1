fd15f0fec23d42e9d5123da1f6b8101e690b104f
Rinad Hamid <rinad.hamid@ucalgary.ca>
2025-10-02 22:30:30 -0600

    Create clkChange.h

---
+++ clkChange.h
@@ -0,0 +1,17 @@
+    /*
+     * File:   clkChange.h
+     * Author: Rinad Hamid, Simar Kandola, Abia Jahangir
+     *
+     * Created on: October 2, 2025
+     */
+
+
+#ifndef CHANGECLK_H
+#define CHANGECLK_H
+
+#include <xc.h>
+
+// Function declarations
+void newClk(unsigned int clkval);
+
+#endif // CHANGECLK_H
