82966f8cef7e6d4d5d9bfed3b91f2933359219c8
Rinad Hamid <rinad.hamid@ucalgary.ca>
2025-10-02 22:29:32 -0600

    Create TimeDelay.h

---
+++ TimeDelay.h
@@ -0,0 +1,17 @@
+    /*
+     * File:   TimeDelay.h
+     * Author: Rinad Hamid, Simar Kandola, Abia Jahangir
+     *
+     * Created on: October 2, 2025
+     */
+
+#ifndef TIMEDELAY_H
+#define TIMEDELAY_H
+
+#include <xc.h>
+#include <stdint.h>
+
+// Function Prototype
+void delay_ms(uint16_t time_ms);
+
+#endif // TIMEDELAY_H
